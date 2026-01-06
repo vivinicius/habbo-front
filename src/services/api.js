1719fa398dc99a3642432f769c36042aaa7ab238
Victor Vinicius <victorvinicius@Mac-mini-de-Victor.local>
2026-01-05 21:07:19 -0300

    teste

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://habbo-api-production.up.railway.app",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 // 🔐 INTERCEPTOR DO TOKEN
